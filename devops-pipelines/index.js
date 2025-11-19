78eb6310e6ccf02c24bdafcc82ee80bdfcfe8035
Mohsin <mohsinsmannat@gmail.com>
2025-11-19 14:14:14 +0530

    feat: add CI pipeline

---
+++ devops-pipelines/index.js
@@ -0,0 +1,2 @@
+console.log('Hello from DevOps Pipelines!');
+console.log("I'm learning about CI/CD processes using GitHub Actions.");
---
+++ devops-pipelines/test.js
@@ -0,0 +1,3 @@
+console.log('Starting DevOps pipeline tests...');
+setTimeout(() => console.log('Waiting for 3 seconds to simulate test execution...'), 3000);
+console.log('All tests passed successfully!');
